fcbfa1fa26bdbc4061116252ef4aa84d68339f02
Ta-Sabir <115227518+Ta-Sabir@users.noreply.github.com>
2022-11-09 19:58:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const icondark=document.getElementById("icondark")
+
+const body=document.getElementById("body")
+
+
+icondark.addEventListener("click" , (eo) => {
+    
+    body.classList.toggle("dark")
+})
+
